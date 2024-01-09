cadbb2a4b6947814ed5be9a52020f1491da8884e
Hayden Chan <hchan@lcs.on.ca>
2024-01-09 14:31:28 -0500

    being able to add more propeties and cauculate the cm of dog using m

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,17 @@
 import Cocoa
 
-var greeting = "Hello, playground"
 struct Dog {
     let breed: String
     let color: String
-    let heightInMetres: Double
+    var heightInMetres: Double
+    let lengthInMetres: Double
+    let massInKilogrames: Double
+    var heightInCentimetres: Double
+    {
+        return heightInMetres * 100
+    }
     
 }
+var Dog1 = Dog(breed: "sd", color: "df", heightInMetres: 45, lengthInMetres: 67, massInKilogrames: 75)
+
+Dog1.heightInCentimetres
